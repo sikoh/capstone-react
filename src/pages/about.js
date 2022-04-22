d2ad2f7fc9b61f21e2b47baa8672bf68c7d0ffc2
Silvana K <84741857+sikoh@users.noreply.github.com>
2022-04-22 15:03:34 -0400

    adding text to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const About = () => {
-    return <h1>Hi</h1>;
+  return (
+    <div className="about-wrapper">
+      <h1>Hi! My name is Silvana 👋 </h1>
+      <p>
+        I've been programming every day for the past 6 months. On the days that
+        I'm not able to directly write code, I read books and watch courses or
+        youtube videos. I don't keep track, but I believe I've watched hundreds
+        of videos about coding. I consider myself a shameless cloner - I try to
+        learn from everyone. I write down everything that makes sense to me and
+        could potentially benefit me and increase my productivity. My view has
+        been - I don't have enough time to figure out a lot of these things on
+        my own, so I've decided to abandon my ego and try to learn as much from
+        others as possible.
+      </p>
+    </div>
+  );
 };
-  
+
 export default About;
